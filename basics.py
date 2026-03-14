e62ee121ca12a66b311c3580c6cdd47726049023
Abdul Basit <basitmehar317@gmail.com>
2026-03-14 23:25:19 +0500

    basics

---
+++ basics.py
@@ -0,0 +1,9 @@
+name = "basit" #string
+age= 12 #integer
+income=12.40 #float
+print("my name is ",name ,"my age is ",age ,"my income is ",income) #concatination
+print(type(name)) #type function is used to check the data type of a variable
+print(type(age))    
+print(type(income))
+print("my name is " + name + " my age is " + str(age) + " my income is " + str(income)) #string concatenation with type conversion using str() function
+
---
+++ checkup.py
@@ -0,0 +1,9 @@
+print("Checkup complete.")
+name = "basit"
+age= 12
+income=12.40
+print("my name is ",name ,"my age is ",age ,"my income is ",income)
+print(type(name))
+print(type(age))    
+print(type(income))
+print("my name is " + name + " my age is " + str(age) + " my income is " + str(income))
